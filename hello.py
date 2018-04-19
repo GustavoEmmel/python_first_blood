ad25a765ba7210153018c0a8dccaa218005efc05
Gustavo Reichelt Emmel <gustavo.emmel@totvs.com.br>
2018-04-19 17:15:54 -0300

    strings

--- hello.py
+++ hello.py
@@ -21,3 +21,23 @@ famous_skectch1 = "\n\t Hell's Grannies"
 
 famous_skectch2 = '\n\t The Dead Parrot'
 print(famous_skectch1, famous_skectch2)
+
+greeting = 'HELLO WORLD!'
+print(greeting[0])
+print(greeting[11])
+print(len(greeting))
+
+word = 'python'
+print(word[2:5])
+
+word1 = 'good'
+word2 = 'evening'
+
+print (word1[2:])
+print(word2[3:])
+
+half1 = len(word1)//2
+half2 = len(word2)//2
+
+print (word1[half1:])
+print(word2[half2:])
