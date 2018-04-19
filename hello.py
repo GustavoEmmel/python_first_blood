b9301cf93ff725ce3a90abaa094c8443c1641850
Gustavo Reichelt Emmel <gustavo.emmel@totvs.com.br>
2018-04-19 14:41:52 -0300

    string variables

--- hello.py
+++ hello.py
@@ -1,1 +1,23 @@
 print("Hello, World!")
+
+first_name = 'Monty'
+last_name = 'Python'
+full_name = first_name + ' ' + last_name
+print(full_name)
+print(first_name, last_name)
+
+#Describe the sketch comedy group
+description = 'sketch comedy group'
+year = 1969
+
+# Introduce then
+sentence = full_name + ' is a ' + description + ' formed in ' + str(year)
+print(sentence)
+
+print (full_name, 'is a ', description, ' formed in ', year)
+
+famous_skectch1 = "\n\t Hell's Grannies"
+# print(famous_skectch1)
+
+famous_skectch2 = '\n\t The Dead Parrot'
+print(famous_skectch1, famous_skectch2)
